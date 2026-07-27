258a7db2e0b017da4a08941043ef44406195dbbd
Rushikesh Kulkarni <rukulkarni4321@gmail.com>
2026-07-27 20:39:23 +0530

    Initial Spring Boot 4.0.0 setup

---
+++ src/test/java/EnterpriseProcurementSystem/ProcurementApplicationTests.java
@@ -0,0 +1,13 @@
+package EnterpriseProcurementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
